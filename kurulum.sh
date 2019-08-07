a8c1e60927ffb16efbc6a57964e27501962fa238
CyberTeamm <52137281+CyberTeamm@users.noreply.github.com>
2019-08-07 11:28:32 +0300

    Add files via upload

---
+++ kurulum.sh
@@ -0,0 +1,19 @@
+echo -e "\e[32m Kurulum Paketi İndiriliyor...\e[0m"
+echo
+echo
+echo
+pkg install git
+pkg install python
+pkg install python2
+pkg install python3
+pkg install nano
+pkg install figlet
+pkg install toilet
+pkg install php
+apt update
+apt upgrade
+pkg install perl
+termux-setup-storage
+exit
+clear
+
